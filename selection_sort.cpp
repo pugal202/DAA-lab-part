cadbf9a9d83ca82886a9467fa9c8deb530c5d226
PUGAL S <pugalshanmugam0@gmail.com>
2026-03-17 08:17:09 +0530

    Added selection sort program

---
+++ selection_sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i, j, minIndex, temp;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements:\n";
+    for(i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Selection Sort
+    for(i = 0; i < n - 1; i++) {
+        minIndex = i;
+
+        for(j = i + 1; j < n; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    cout << "Sorted array:\n";
+    for(i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
